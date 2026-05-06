c7cab271be6e56c84da3c231c38ebfe9fe7593a1
Saurabh Kumbhar <45506009+SaurabhKumbhar24@users.noreply.github.com>
2026-05-05 19:44:18 -0600

    Added OAuth to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -24,6 +24,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useAppDispatch, useAppSelector } from "@/store/hooks"
 import { clearError, loginThunk } from "@/store/auth/slice"
+import { API_BASE_URL } from "@/lib/api"
 
 function Login() {
   const dispatch = useAppDispatch()
@@ -86,13 +87,13 @@ function Login() {
             <CardContent className="gap-4">
               <div className="grid gap-2">
                 <Button variant="outline" className="w-full" asChild>
-                  <a href="#">
+                  <a href={`${API_BASE_URL}/auth/oauth/github`}>
                     <HugeiconsIcon icon={Github01Icon} />
                     Continue with GitHub
                   </a>
                 </Button>
                 <Button variant="outline" className="w-full" asChild>
-                  <a href="#">
+                  <a href={`${API_BASE_URL}/auth/oauth/google`}>
                     <HugeiconsIcon icon={GoogleIcon} />
                     Continue with Google
                   </a>
